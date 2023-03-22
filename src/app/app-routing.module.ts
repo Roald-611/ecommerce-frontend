3ef595b3237857186b9a3c5b524afdaba1d33060
Kana Varu <kana@lectus.tech>
2023-03-22 22:51:35 +0530

    Complete standard front end of ecommerce

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import {ProductComponent} from "./components/product/product.component";
+import {CartComponent} from "./components/cart/cart.component";
+import {ChackoutComponent} from "./components/chackout/chackout.component";
+import {ThankyouComponent} from "./components/thankyou/thankyou.component";
+import {LoginComponent} from "./components/login/login.component";
+import {ProfileGuard} from "./guard/profile.guard";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'product/:id',component:ProductComponent},
+  {path:'cart',component:CartComponent},
+  {path:'checkout',component:ChackoutComponent},
+  {path:'thankyou',component:ThankyouComponent},
+  {path:'login',component:LoginComponent},
+  {path:'profile',component:ProductComponent,canActivate:[ProfileGuard]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
